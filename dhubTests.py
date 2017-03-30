963aa9e1a3c0d074838ba4bd1778d579d714559c
gluaxspeed <jonathan.t.pavlik@gmail.com>
2017-03-29 21:57:49 -0400

    finished writing test cases for website

--- dhubTests.py
+++ dhubTests.py
@@ -2,48 +2,93 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import string
+import random
 
 class dHubTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Chrome()
-    
+
     def test_search(self):
+        '''a good thing to automatically test because you can automate and make sure correct
+        search results appear'''
         self.driver.get('https://hub.docker.com/')
         search_bar = self.driver.find_element_by_class_name('SearchBar__searchInput___34nC3')
         search_bar.send_keys('nginx')
         search_bar.send_keys(Keys.ENTER)
         time.sleep(2)
-        repNumber = self.driver.find_element_by_class_name('left')
-        self.assertEqual(repNumber.text, 'Repositories (16426)')
+        offical = self.driver.find_element_by_class_name('undefined')
+        self.assertEqual(offical.text, 'official')
     
     def test_sign_up_fail_uname(self):
+        '''another good thing to automate to make sure no invalid usernames can be used
+        to make an account'''
         self.driver.get('https://hub.docker.com/')
-        uname = self.driver.find_element_by_name('Choose a Docker ID')
+        uname = self.driver.find_element_by_xpath('//form/div[1]/div/input')
         uname.send_keys('txt')
         e = self.driver.find_element_by_class_name('FancyInput__error___TIz2p')
         self.assertEqual(e.text, 'Username must be at least four characters long')
-    '''
-    def test_sign_up_fail_email(self):
+       
+    def test_sign_up_fail_passw(self):
+        '''once again automate to make sure no possible invalid passwords can be 
+        used to make an account'''
         self.driver.get('https://hub.docker.com/')
-        email = self.driver.find_element_by_class_name('FancyInput__default___1Iybp')
-        email.send_keys('txt')
-        button = self.driver.find_element_by_class_name('Button__button___2lhyK Button__variant-primary___3QTAR')
-        button.click()
-        e = self.driver.find_element_by_class_name('')
-        self.assertEqual(e.text, '')
-    
-    def test_sign_up_fail_pass(self):
-        self.driver.get('https://hub.docker.com/')
-        passw = self.driver.find_element_by_name('password')
+        passw = self.driver.find_element_by_xpath('//form/div[3]/div/input')
         passw.send_keys('test')
-        button = self.driver.find_element_by_class_name('Button__button___2lhyK Button__variant-primary___3QTAR')
+        self.driver.find_element_by_xpath('//form/div[4]/button').click()
+        time.sleep(1)
+        e = self.driver.find_element_by_xpath('//form/div[3]/div/div')
+        self.assertEqual(e.text, 'Ensure this value has at least 6 characters (it has 4).')
+    
+    def test_sign_up(self):
+        '''make sure a proper account names and user names and emails can be used to make an account '''
+        self.driver.get('https://hub.docker.com/')
+        uname = self.driver.find_element_by_xpath('//form/div[1]/div/input')
+        uname.send_keys('tester'+''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5)))
+        email = self.driver.find_element_by_xpath('//form/div[2]/div/input')
+        email.send_keys(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(7))+'@gmail.com')
+        passw = self.driver.find_element_by_xpath('//form/div[3]/div/input')
+        passw.send_keys('qwertyuiop')
+        self.driver.find_element_by_xpath('//form/div[4]/button').click()
         time.sleep(2)
-        #button.click()
-        #e = self.driver.find_element_by_class_name('FancyInput__error___TIz2p')
-        #self.assertEqual(e.text, 'Ensure this value has at least 6 characters (it has 3).')
-    '''  
+        sutext = self.driver.find_element_by_class_name('SignupForm__subtext___1JThA')
+        self.assertEqual(sutext.text, 'Please check your email to activate your account.')
+
+        
+    def test_log_in_fail(self):''' '''
+        '''good to automate as you never want someone to be able to log in
+        when incorrect information is entered'''
+        self.driver.get('https://hub.docker.com/')
+        self.driver.find_element_by_xpath('//section/ul[3]/li[3]/a').click()
+        uname = self.driver.find_element_by_xpath('//form/div[1]/input')
+        uname.send_keys('tester568')
+        passw = self.driver.find_element_by_xpath('//form/div[2]/input')
+        passw.send_keys('qwertyuiop')
+        self.driver.find_element_by_xpath('//form/button').click()
+        time.sleep(1)
+        e = self.driver.find_element_by_class_name('styles__error__1v2q-')
+        #self.assertEqual(e, 'Incorrect authentication credentials.')
+    
+    def test_log_in(self):
+        '''test to make sure accounts can properly log in and so that users will
+        never have an issue logging in'''
+        self.driver.get('https://hub.docker.com/')
+        self.driver.find_element_by_xpath('//section/ul[3]/li[3]/a').click()
+        uname = self.driver.find_element_by_xpath('//form/div[1]/input')
+        uname.send_keys('tester5683')
+        passw = self.driver.find_element_by_xpath('//form/div[2]/input')
+        passw.send_keys('qwertyuiop')
+        self.driver.find_element_by_xpath('//form/button').click()
+        time.sleep(1)
+        confirm = self.driver.find_element_by_xpath('//h1').text
+        expand = self.driver.find_element_by_xpath('//section/ul[3]/li[5]/a')
+        expand.click()
+        logout = self.driver.find_element_by_xpath('//section/ul[3]/li[5]/ul/li[7]/a')
+        logout.click()
+        self.assertEqual(confirm, 'Welcome to Docker Hub')
+       
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
--- tests.py
+++ tests.py
@@ -2,14 +2,23 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import string
+import random
 
 driver = webdriver.Chrome()
 driver.get('https://hub.docker.com/')
-input = driver.find_element_by_xpath('//form/div[1]/div/input')
-input.send_keys('test')
-#input = driver.find_element_by_xpath('//input[3]')
-#input.send_keys('3')
-#input = driver.find_element_by_xpath('//input[4]')
-#input.send_keys('4')
+driver.get('https://hub.docker.com/')
+driver.find_element_by_xpath('//section/ul[3]/li[3]/a').click()
+uname = driver.find_element_by_xpath('//form/div[1]/input')
+uname.send_keys('tester5683')
+passw = driver.find_element_by_xpath('//form/div[2]/input')
+passw.send_keys('qwertyuiop')
+driver.find_element_by_xpath('//form/button').click()
+rawr = driver.find_element_by_xpath('//h1')
+print(rawr.text)
+time.sleep(5)
+driver.find_element_by_xpath('//section/ul[3]/li[5]/a').click()
+time.sleep(1)
+driver.find_element_by_xpath('//section/ul[3]/li[5]/ul/li[7]/a').click()
 time.sleep(5)
 driver.quit()
